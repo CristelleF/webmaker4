b202f54132f395353b339ebedbe5598d5e948523
CristelleF <telmesumting@gmail.com>
2019-05-20 14:27:02 -0400

    2nd try

--- server/services/user.server.js
+++ server/services/user.server.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
         {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email: "alice@gmail.com"},
         {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email: "bob@whatever.com"},
         {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia", email: "charly@ulem.com"},
-        {_id: "456", username: "shiyu", password: "shiyu", firstName: "Shiyu", lastName: "Wang", email: "swang@ulem.org"}
+        {_id: "456", username: "shiyu", password: "shiyu", firstName: "Shiyu", lastName: "Wang", email: "swang@ulem.org"},
+        {_id: "274", username:"Cristelle", password:"Cris", firstName:"Cristelle", lastName:"Fofana", email:"telmesumting@gmail.com"}
     ]
 
     // Find users by username and password
